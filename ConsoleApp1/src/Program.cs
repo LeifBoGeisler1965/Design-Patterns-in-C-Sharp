27c8c91fb9b1fc463a502f7d02c2030b33d8e7af
Leif Bo Geisler <leifbogeisler@gmail.com>
2026-02-24 22:40:40 +0100

    ChainOfResponsibility

--- ConsoleApp1/src/Program.cs
+++ ConsoleApp1/src/Program.cs
@@ -1,1 +1,16 @@
-﻿System.Console.WriteLine("Hello world");
+﻿
+#region Behavioral.ChainOfResponsibility
+using ConsoleApp1.src.DesignPatterns.Behavioral.ChainOfResponsibility.MyTest;
+
+var validator = new Validator();
+var authenticator = new Authenticator();
+var logger = new Logger();
+
+validator.SetNext(authenticator).SetNext(logger);
+
+WebServer webServer = new WebServer(validator);
+HTTPRequest request = new HTTPRequest("Leif", "123");
+webServer.Handle(request);
+
+#endregion
+Console.ReadLine();
