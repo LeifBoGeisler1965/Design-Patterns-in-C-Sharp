6d89c2653a2b99c64e9efff306bcbab46395d849
Leif Bo Geisler <leifbogeisler@gmail.com>
2026-02-25 09:21:17 +0100

    Iterator

--- ConsoleApp1/src/Program.cs
+++ ConsoleApp1/src/Program.cs
@@ -1,16 +1,47 @@
 ﻿
-#region Behavioral.ChainOfResponsibility
-using ConsoleApp1.src.DesignPatterns.Behavioral.ChainOfResponsibility.MyTest;
+#region Behavioral Pattern
 
-var validator = new Validator();
-var authenticator = new Authenticator();
-var logger = new Logger();
+#region Command
 
-validator.SetNext(authenticator).SetNext(logger);
+#endregion
 
-WebServer webServer = new WebServer(validator);
-HTTPRequest request = new HTTPRequest("Leif", "123");
-webServer.Handle(request);
+#region Iterator
+using ConsoleApp1.src.DesignPatterns.Behavioral.Iterator.MyTest;
+using System.Runtime.CompilerServices;
+ShoppingList shoppingList = new ShoppingList();
+shoppingList.Push("Bread");
+shoppingList.Push("Milk");
+shoppingList.Push("Cola");
 
+var iterator = shoppingList.CreateIterator();
+
+PrintList(shoppingList);
+shoppingList.Pop();
+PrintList(shoppingList);
+void PrintList(ShoppingList shoppingList)
+{
+    iterator.Reset();
+    while (iterator.HasNext())
+    {
+        Console.WriteLine(iterator.Current());
+        iterator.Next();
+    }
+}
+#endregion
+
+#region ChainOfResponsibility
+//using ConsoleApp1.src.DesignPatterns.Behavioral.ChainOfResponsibility.MyTest;
+
+//var validator = new Validator();
+//var authenticator = new Authenticator();
+//var logger = new Logger();
+
+//validator.SetNext(authenticator).SetNext(logger);
+
+//WebServer webServer = new WebServer(validator);
+//HTTPRequest request = new HTTPRequest("Leif", "123");
+//webServer.Handle(request);
+
+#endregion
 #endregion
 Console.ReadLine();
